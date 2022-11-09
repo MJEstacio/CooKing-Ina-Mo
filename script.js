2e10d367a79643dbcf4f68c6cd4833270a4546d2
Mark Joseph Estacio <mjestacioiii@gmail.com>
2022-11-10 00:08:58 +0800

    Cooking Ina

---
+++ script.js
@@ -0,0 +1,61 @@
+var burger = document.querySelector('.nav-btn')
+var navLinks = document.querySelector('nav ul')
+
+burger.addEventListener('click', function () {
+  navLinks.classList.toggle('show')
+})
+
+// Scripts for Image Slider
+var slides = document.querySelectorAll('.slide')
+var btns = document.querySelectorAll('.btn')
+let currentSlide = 1
+
+// Javascript for image slider manual navigation
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+
+    btns.forEach((btn) => {
+      btn.classList.remove('active')
+    })
+  })
+
+  slides[manual].classList.add('active')
+  btns[manual].classList.add('active')
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    manualNav(i)
+    currentSlide = i
+  })
+})
+
+// Javascript for image slider autoplay navigation
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName('active')
+  let i = 1
+
+  var repeater = () => {
+    setTimeout(function () {
+      ;[...active].forEach((activeSlide) => {
+        activeSlide.classList.remove('active')
+      })
+
+      slides[i].classList.add('active')
+      btns[i].classList.add('active')
+      i++
+
+      if (slides.length == i) {
+        i = 0
+      }
+      if (i >= slides.length) {
+        return
+      }
+      repeater()
+    }, 10000)
+  }
+  repeater()
+}
+repeat()
+// Scripts for Image Slider
